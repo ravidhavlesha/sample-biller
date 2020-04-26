f6833f86afd5ae23f24d120d8c8cb22d98369eb2
Ravi Dhavlesha <ravidhavlesha@gmail.com>
2020-04-26 13:10:23 +0530

    feat: add audience in jwt verify

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,7 +1,8 @@
 const jwt = require('express-jwt');
 
 const secretKey = process.env.JWT_SECRET_KEY || '';
+const schemeID = process.env.JWT_SCHEME_ID || '';
 
-const jwtAuth = jwt({ secret: secretKey });
+const jwtAuth = jwt({ secret: secretKey, audience: schemeID });
 
 module.exports = jwtAuth;
